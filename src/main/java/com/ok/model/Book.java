64852edee824abab601ff8f9697cd42a9004fb44
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-01-01 06:43:44 +0500

    Add Book entity

---
+++ src/main/java/com/ok/model/Book.java
@@ -0,0 +1,80 @@
+package com.ok.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.AssertTrue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String isbn;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String author;
+
+	@JoinColumn(nullable = false)
+	@ManyToOne
+	private Genre genre;
+
+	private String publisher;
+
+	private LocalDate publishDate;
+
+	private String language;
+
+	private Integer pages;
+
+	private String description;
+
+	@Column(nullable = false)
+	private Integer totalCopies;
+
+	@Column(nullable = false)
+	private Integer availableCopies;
+
+	private BigDecimal price;
+
+	private String coverImageUrl;
+
+	@Column(nullable = false)
+	private Boolean active=true;
+
+	@CreationTimestamp
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
+
+	@AssertTrue(message = "Available copies cannot exceed total copies!")
+	public boolean isAvailableCopiesValid() {
+
+		if (totalCopies == null || availableCopies == null) {
+			//return false;
+			return true;
+		}
+		return availableCopies <= totalCopies;
+	}
+}
