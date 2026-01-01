9b7eac482b93b65f490eb035af1f661876805996
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-01-01 10:24:15 +0500

    Add Book repository and Book Service implementation

---
+++ src/main/java/com/ok/repo/BookRepo.java
@@ -0,0 +1,34 @@
+package com.ok.repo;
+
+import com.ok.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface BookRepo extends JpaRepository<Book,Long> {
+
+	Optional<Book> findByIsbn(String isbn);
+
+	boolean existsByIsbn(String isbn);
+
+	//? book="java programming" - java, prog OK, progress not OK
+
+	@Query("select b from Book b where " +
+					":searchTerm is null OR " +
+					"lower(b.title) like lower(concat ('%', :searchTerm, '%')) OR " +
+					"lower(b.author) like lower(concat ('%', :searchTerm, '%')) OR " +
+					"lower(b.isbn) like lower(concat ('%', :searchTerm, '%')) OR " +
+					"(:genreId is null OR b.genre.id=:genreId) AND " +
+					"(:availableOnly == false OR b.availableCopies>0) AND " +
+					"b.active=true"
+	)
+	Page<Book> searchBooksWithFilters(@Param("searchTerm") String searchTerm,
+	                                  @Param("genreId") Long genreId,
+	                                  @Param("availableOnly") boolean availableOnly,
+	                                  Pageable pageable);
+
+}
---
+++ src/main/java/com/ok/service/impl/BookServiceImpl.java
@@ -0,0 +1,67 @@
+package com.ok.service.impl;
+
+import com.ok.payload.dto.BookDTO;
+import com.ok.payload.request.BookSearchRequest;
+import com.ok.payload.response.PageResponse;
+import com.ok.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+
+
+	@Override
+	public BookDTO createBook(BookDTO bookDTO) {
+		return null;
+	}
+
+	@Override
+	public List<BookDTO> createBooksBulk() {
+		return List.of();
+	}
+
+	@Override
+	public BookDTO getBookById(Long bookId) {
+		return null;
+	}
+
+	@Override
+	public BookDTO getBookByISBN(String isbn) {
+		return null;
+	}
+
+	@Override
+	public BookDTO updateBook(Long bookId, BookDTO bookDTO) {
+		return null;
+	}
+
+	@Override
+	public void deleteBook(Long bookId) {
+
+	}
+
+	@Override
+	public void hardDeleteBook(Long bookId) {
+
+	}
+
+	@Override
+	public PageResponse<BookDTO> searchBooksWithFilters(BookSearchRequest searchRequest) {
+		return null;
+	}
+
+	@Override
+	public long getTotalActiveBooks() {
+		return 0;
+	}
+
+	@Override
+	public long getTotalAvailableBooks() {
+		return 0;
+	}
+}
