8549f8d1ec6f36b8ea94c2f4bcf569f46c4052e5
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-01-06 16:27:08 +0500

    Add BookLoan Service

--- src/main/java/com/ok/service/BookLoanService.java
+++ src/main/java/com/ok/service/BookLoanService.java
@@ -1,8 +1,11 @@
 package com.ok.service;
 
+import com.ok.domain.BookLoanStatus;
 import com.ok.payload.dto.BookLoanDTO;
 import com.ok.payload.request.CheckinRequest;
 import com.ok.payload.request.CheckoutRequest;
+import com.ok.payload.request.RenewalRequest;
+import com.ok.payload.response.PageResponse;
 
 public interface BookLoanService {
 
@@ -13,5 +16,10 @@ public interface BookLoanService {
 	BookLoanDTO checkinBookBook(CheckinRequest checkinRequest);
 
 	BookLoanDTO renewCheckout(RenewalRequest renewalRequest);
+
+	PageResponse<BookLoanDTO> getMyBookLoans(
+					BookLoanStatus status, int page, int size);
+
+	PageResponse<BookLoanDTO> getBookLoans(BookLoanSearchRequest request);
 
 }
