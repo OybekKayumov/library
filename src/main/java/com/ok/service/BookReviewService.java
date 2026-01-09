a447d7bbe9d62e7fe22ce64d11d92988f308b96e
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-01-09 12:06:00 +0500

    Add Book Review

--- src/main/java/com/ok/service/BookReviewService.java
+++ src/main/java/com/ok/service/BookReviewService.java
@@ -2,6 +2,9 @@ package com.ok.service;
 
 import com.ok.payload.dto.BookReviewDTO;
 import com.ok.payload.request.CreateReviewRequest;
+import com.ok.payload.request.UpdateReviewRequest;
+import com.ok.payload.response.PageResponse;
+import org.springframework.data.domain.PageRequest;
 
 public interface BookReviewService {
 
@@ -9,5 +12,7 @@ public interface BookReviewService {
 
 	BookReviewDTO updateReview(Long reviewId, UpdateReviewRequest request);
 
+	void deleteReview(Long reviewId);
 
+	PageResponse<BookReviewDTO> getReviewsByBookId(Long id, int page, int size);
 }
